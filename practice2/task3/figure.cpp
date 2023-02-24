013e8f8b3e306c14a97459c73debb864af089029
AlexDechart <112802973+AlexDechart@users.noreply.github.com>
2023-02-24 23:34:35 +0300

    Create figure.cpp

---
+++ practice2/task3/figure.cpp
@@ -0,0 +1,55 @@
+#include "figure.h"
+using namespace std;
+figure::figure(float X1, float X2, float X3, float X4, float Y1, float Y2, float Y3, float Y4)
+{
+	x1 = X1;
+	x2 = X2;
+	x3 = X3;
+	x4 = X4;
+	y1 = Y1;
+	y2 = Y2;
+	y3 = Y3;
+	y4 = Y4;
+	si1 = ca_side(x1, x2, y1, y2);
+	si2 = ca_side(x2, x3, y2, y3);
+	si3 = ca_side(x3, x4, y3, y4);
+	si4 = ca_side(x1, x4, y1, y4);
+	float diag1 = ca_side(x1, x3, y1, y3);
+	float diag2 = ca_side(x2, x4, y2, y4);
+	p = si1 + si2 + si3 + si4;
+	si1 = ca_side(x1, x2, y1, y2);
+	float co_sum = (x1 + x3) * (x2 + x4) + (y1 * y3) * (y2 * y4);
+	float co_def = sqrt(pow(x1 + x3, 2) + pow(y1 + y3, 2)) * sqrt(pow(x2 + x4, 2) + pow(y2 + y4, 2));
+	float cos = co_sum / co_def;
+	float sin = sqrt(abs(1 - pow(cos, 2)));
+	s = diag1 * diag2 * sin * 0.5;
+}
+void figure::show() {
+	cout << "сторона " << si1 << ", " << si2 << ", " << si3 << ", " << si4 << ";" << endl;
+	cout << "площадь " << s << endl;
+	cout << "периметр " << p << endl;
+}
+bool figure::is_prug() {
+	return (s / (si1 * si2) >= 0.95 && s / (si1 * si2) <= 1.05);
+}
+bool figure::is_square() {
+	bool is_pr = is_prug();
+	return (si1 == si2 == si3 == si4 && is_pr);
+}
+bool figure::is_romb() {
+	bool is_pr = is_prug();
+	if (si1 == si2 == si3 == si4 && is_pr == false) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool figure::is_in_circle() {
+	float P1 = p / 2;
+	float S1 = sqrt((P1 - si1) * (P1 - si2) * (P1 - si3) * (P1 - si4));
+	return (S1 == s);
+}
+bool figure::is_out_circle() {
+	return (si1 + si3 == si2 + si4);
+}
